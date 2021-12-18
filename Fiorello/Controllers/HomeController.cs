d2a803fe534f7853fe6ab67b35a0d323e1ec6f97
QudretCA <tu201906302@code.edu.az>
2021-12-18 02:34:39 -0800

    Add project files.

---
+++ Fiorello/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Fiorello.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
