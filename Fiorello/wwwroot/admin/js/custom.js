a24d10ee7ba9917b1e73a43e1b3bc272d2ef88e9
QudretCA <tu201906302@code.edu.az>
2021-12-28 17:50:36 -0800

    AdminPanel-fixed/SliderImageEmptyRequest

--- Fiorello/wwwroot/admin/js/custom.js
+++ Fiorello/wwwroot/admin/js/custom.js
@@ -15,9 +15,9 @@ $(function () {
     $('#photoUpload').change(
         function () {
             var files = $('input[type="file"]')[0].files;
-            if (files.length > 5) {
+            if (files.length > 5 || files.length == 0) {
                 $('#btnUpload').addClass('d-none');
-                $('#myLabel').html("*5 dən çox şəkil yükləmək olmaz.");
+                $('#myLabel').html("*Maksimum 5, minimum 1 şəkil yüklənməlidir");
             }
             else {
                 $('#btnUpload').removeClass('d-none');
